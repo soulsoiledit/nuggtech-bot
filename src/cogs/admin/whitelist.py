b4efd47083879453bcec16cd9ae6047d398ca4a4
soulsoiledit <aryankevat@proton.me>
2023-07-24 17:39:36 -0500

    Revamp manage and whitelist

--- src/cogs/admin/whitelist.py
+++ src/cogs/admin/whitelist.py
@@ -3,16 +3,8 @@ from discord.ext import commands
 from discord import TextChannel, app_commands
 from discord.app_commands import Choice
 
-from main import bot_
 import bot
-from bridge import bridge_send
-
-member_role = bot_.discord_config.member_role
-admin_role = bot_.discord_config.admin_role
-server_choices = [
-    Choice(name=server.display_name, value=server.name)
-    for server in bot_.server_config
-]
+from bridge import bridge_send, handle_whitelist
 
 class Whitelist(commands.Cog):
     def __init__(self, bot: bot.PropertyBot):
@@ -24,28 +16,40 @@ class Whitelist(commands.Cog):
     )
 
     @whitelist_commands.command(name="add", description="Adds a user to a server's whitelist")
-    @app_commands.describe(server="the server to target")
-    @app_commands.describe(server="the user to whitelist")
-    @app_commands.choices(server=server_choices)
-    @app_commands.checks.has_role(admin_role)
+    @app_commands.default_permissions(administrator=True)
+    @app_commands.describe(server="target server")
+    @app_commands.describe(user="target user")
+    @app_commands.choices(server=bot.server_choices)
     async def whitelist_add(self, interaction: discord.Interaction, server: Choice[str], user: str):
-        await bridge_send(self.bot, server.value, f"CMD {server.value} whitelist add {user}")
-        log_channel = self.bot.get_channel(self.bot.discord_config.log_channel)
-        if isinstance(log_channel, TextChannel):
-            await log_channel.send(f"Added {user} to {server.value}")
-        await interaction.response.send_message(f'Added {user} to {server.value}!', ephemeral=True)
+        target = server.value
+        await interaction.response.defer(ephemeral=True)
+        await bridge_send(self.bot.servers, target, f"RCON {target} whitelist add {user}")
+        result = await handle_whitelist(self.bot.response_queue)
+        if result[0]:
+            await interaction.followup.send(f"Added `{user}` to `{target}`")
+            log_channel = self.bot.get_channel(self.bot.discord_config.log_channel)
+            if isinstance(log_channel, TextChannel):
+                await log_channel.send(f"`{interaction.user.name}` added `{user}` to `{target}`")
+        else:
+            await interaction.followup.send(result[1])
 
     @whitelist_commands.command(name="remove", description="Removes a user from a server's whitelist")
-    @app_commands.describe(server="the server to target")
-    @app_commands.describe(server="the user to unwhitelist")
-    @app_commands.choices(server=server_choices)
-    @app_commands.checks.has_role(admin_role)
+    @app_commands.default_permissions(administrator=True)
+    @app_commands.describe(server="target server")
+    @app_commands.describe(user="target user")
+    @app_commands.choices(server=bot.server_choices)
     async def whitelist_remove(self, interaction: discord.Interaction, server: Choice[str], user: str):
-        await bridge_send(self.bot, server.value, f"CMD {server.value} whitelist remove {user}")
-        log_channel = self.bot.get_channel(self.bot.discord_config.log_channel)
-        if isinstance(log_channel, TextChannel):
-            await log_channel.send(f"Removed {user} from {server.value}")
-        await interaction.response.send_message(f'Removed {user} from {server.value}!', ephemeral=True)
+        target = server.value
+        await interaction.response.defer(ephemeral=True)
+        await bridge_send(self.bot.servers, target, f"RCON {target} whitelist remove {user}")
+        result = await handle_whitelist(self.bot.response_queue)
+        if result[0]:
+            await interaction.followup.send(f"Removed `{user}` from `{target}`")
+            log_channel = self.bot.get_channel(self.bot.discord_config.log_channel)
+            if isinstance(log_channel, TextChannel):
+                await log_channel.send(f"`{interaction.user.name}` removed `{user}` from `{target}`")
+        else:
+            await interaction.followup.send(result[1])
 
 async def setup(bot: bot.PropertyBot):
     await bot.add_cog(Whitelist(bot))
