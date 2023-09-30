fa9e8d715c06287be0fbcb55ad3d2348aeadc46a
soulsoiledit <30585327+soulsoiledit@users.noreply.github.com>
2023-09-30 00:26:36 -0500

    Formatting

--- src/main.py
+++ src/main.py
@@ -1,29 +1,26 @@
 import logging, argparse
-import discord
 from bot import PropertyBot
+
 
 def main():
     parser = argparse.ArgumentParser(
         description="Discord bot for the NuggTech server",
         epilog="uwu :3",
-        allow_abbrev=False
+        allow_abbrev=False,
     )
-    parser.add_argument("--config", help="specify configuration file", default="./config.toml")
-    parser.add_argument("--verbosity", help="set verbosity level")
+    parser.add_argument(
+        "--config", help="specify configuration file", default="./config.toml"
+    )
+    parser.add_argument("--verbosity", help="set verbosity level", default="warning")
     args = parser.parse_args()
 
     configfile = args.config
 
-    if args.verbosity:
-        logger = logging.getLogger("nuggtech-bot")
-        logger.setLevel(level=getattr(logging, args.verbosity.upper()))
-        handler = logging.StreamHandler()
-        handler.setFormatter(discord.utils._ColourFormatter())
-        logger.addHandler(handler)
-
     bot = PropertyBot(configfile)
 
-    bot.run(bot.discord_config.token)
+    log_level = getattr(logging, args.verbosity.upper())
+    bot.run(bot.discord_config.token, log_level=log_level)
+
 
 if __name__ == "__main__":
     main()
