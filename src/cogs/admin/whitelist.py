850a5b777fe0aff5472dac228c67426290fdb8d0
soulsoiledit <30585327+soulsoiledit@users.noreply.github.com>
2023-09-29 23:58:22 -0500

    Add op commands

--- src/cogs/admin/whitelist.py
+++ src/cogs/admin/whitelist.py
@@ -82,5 +82,71 @@ class Whitelist(commands.Cog):
         else:
             await interaction.followup.send(result[1])
 
+    op_commands = app_commands.Group(
+        name="op", description="Add and remove players from the op"
+    )
+
+    async def handle_op(self) -> tuple[bool, str]:
+        response = await self.bot.response_queue.get()
+        success = "Nothing changed." not in response
+        return (success, response)
+
+    @op_commands.command(name="add", description="Adds a user to a server's op users")
+    @app_commands.default_permissions(administrator=True)
+    @app_commands.describe(server="target server")
+    @app_commands.choices(server=bot.creative_server_choices)
+    @app_commands.describe(user="target user")
+    async def op_add(
+        self,
+        interaction: discord.Interaction,
+        server: app_commands.Choice[str],
+        user: str,
+    ):
+        target = server.value
+        await interaction.response.defer(ephemeral=True)
+        await bridge_send(self.bot.servers, target, f"RCON {target} op {user}")
+        result = await self.handle_op()
+        if result[0]:
+            await interaction.followup.send(f"Op'd `{user}` on {target}!")
+            log_channel = await self.bot.fetch_channel(
+                self.bot.discord_config.log_channel
+            )
+            if isinstance(log_channel, discord.TextChannel):
+                await log_channel.send(
+                    f"`{interaction.user.name}` op'd `{user}` on {target}"
+                )
+        else:
+            await interaction.followup.send(result[1])
+
+    @op_commands.command(
+        name="remove", description="Removes a user from a server's op users"
+    )
+    @app_commands.default_permissions(administrator=True)
+    @app_commands.describe(server="target server")
+    @app_commands.choices(server=bot.creative_server_choices)
+    @app_commands.describe(user="target user")
+    async def op_remove(
+        self,
+        interaction: discord.Interaction,
+        server: app_commands.Choice[str],
+        user: str,
+    ):
+        target = server.value
+        await interaction.response.defer(ephemeral=True)
+        await bridge_send(self.bot.servers, target, f"RCON {target} deop {user}")
+        result = await self.handle_op()
+        if result[0]:
+            await interaction.followup.send(f"Deop'd `{user}` on {target}!")
+            log_channel = await self.bot.fetch_channel(
+                self.bot.discord_config.log_channel
+            )
+            if isinstance(log_channel, discord.TextChannel):
+                await log_channel.send(
+                    f"`{interaction.user.name}` deop'd `{user}` on {target}"
+                )
+        else:
+            await interaction.followup.send(result[1])
+
+
 async def setup(bot: bot.PropertyBot):
     await bot.add_cog(Whitelist(bot))
