296d6b5650bd277deeb12a43cd7a8fa2ca2e9b7f
soiledit <no.sleep410@passinbox.com>
2025-06-25 23:15:08 -0500

    update whitelist

--- src/cogs/admin/whitelist.py
+++ src/cogs/admin/whitelist.py
@@ -1,152 +1,73 @@
-import discord
-from discord.ext import commands
-from discord import app_commands
+from typing import Literal
 
-import bot
-from bridge import bridge_send
+from discord import Interaction
+from discord.app_commands import command, default_permissions
+from discord.ext import commands
+
+from bot import NuggTechBot, Servers
 
 
 class Whitelist(commands.Cog):
-    def __init__(self, bot: bot.PropertyBot):
-        self.bot = bot
+  def __init__(self, bot: NuggTechBot):
+    self.bot: NuggTechBot = bot
 
-    whitelist_commands = app_commands.Group(
-        name="whitelist", description="Add and remove players from the whitelist"
-    )
+  @command(description="whitelist players")
+  @default_permissions()
+  async def whitelist(
+    self,
+    inter: Interaction,
+    server: Servers,
+    action: Literal["add", "remove"],
+    user: str,
+  ):
+    await inter.response.defer(ephemeral=True)
 
-    async def handle_whitelist(self) -> tuple[bool, str]:
-        response = await self.bot.response_queue.get()
-        success = response.split()[0]
-        success = success == "Added" or success == "Removed"
-        return (success, response)
+    bridge, server_ = server.value
+    response = await bridge.sendr(f"RCON {server_} whitelist {action} {user}")
 
-    @whitelist_commands.command(
-        name="add", description="Adds a user to a server's whitelist"
-    )
-    @app_commands.default_permissions(administrator=True)
-    @app_commands.describe(server="target server")
-    @app_commands.choices(server=bot.server_choices)
-    @app_commands.describe(user="target user")
-    async def whitelist_add(
-        self,
-        interaction: discord.Interaction,
-        server: app_commands.Choice[str],
-        user: str,
-    ):
-        target = server.value
-        await interaction.response.defer(ephemeral=True)
-        await bridge_send(
-            self.bot.servers, target, f"RCON {target} whitelist add {user}"
-        )
-        result = await self.handle_whitelist()
-        if result[0]:
-            await interaction.followup.send(f"Added `{user}` to {target}!")
-            log_channel = await self.bot.fetch_channel(
-                self.bot.discord_config.log_channel
-            )
-            if isinstance(log_channel, discord.TextChannel):
-                await log_channel.send(
-                    f"`{interaction.user.name}` added `{user}` to {target}"
-                )
-        else:
-            await interaction.followup.send(result[1])
+    if response.endswith("whitelist"):
+      if response.startswith("Added"):
+        verb = "Whitelisted"
+      else:
+        verb = "Unwhitelisted"
 
-    @whitelist_commands.command(
-        name="remove", description="Removes a user from a server's whitelist"
-    )
-    @app_commands.default_permissions(administrator=True)
-    @app_commands.describe(server="target server")
-    @app_commands.choices(server=bot.server_choices)
-    @app_commands.describe(user="target user")
-    async def whitelist_remove(
-        self,
-        interaction: discord.Interaction,
-        server: app_commands.Choice[str],
-        user: str,
-    ):
-        target = server.value
-        await interaction.response.defer(ephemeral=True)
-        await bridge_send(
-            self.bot.servers, target, f"RCON {target} whitelist remove {user}"
-        )
-        result = await self.handle_whitelist()
-        if result[0]:
-            await interaction.followup.send(f"Removed `{user}` from {target}!")
-            log_channel = await self.bot.fetch_channel(
-                self.bot.discord_config.log_channel
-            )
-            if isinstance(log_channel, discord.TextChannel):
-                await log_channel.send(
-                    f"`{interaction.user.name}` removed `{user}` from {target}"
-                )
-        else:
-            await interaction.followup.send(result[1])
+      await self.bot.log(
+        f"`{inter.user}` {verb.lower()} `{user}` on {server_.display}",
+      )
 
-    op_commands = app_commands.Group(
-        name="op", description="Add and remove players from the op"
-    )
+      response = f"{verb} {user} on {server_.display}"
 
-    async def handle_op(self) -> tuple[bool, str]:
-        response = await self.bot.response_queue.get()
-        success = "Nothing changed." not in response
-        return (success, response)
+    await inter.followup.send(response)
 
-    @op_commands.command(name="add", description="Adds a user to a server's op users")
-    @app_commands.default_permissions(administrator=True)
-    @app_commands.describe(server="target server")
-    @app_commands.choices(server=bot.creative_server_choices)
-    @app_commands.describe(user="target user")
-    async def op_add(
-        self,
-        interaction: discord.Interaction,
-        server: app_commands.Choice[str],
-        user: str,
-    ):
-        target = server.value
-        await interaction.response.defer(ephemeral=True)
-        await bridge_send(self.bot.servers, target, f"RCON {target} op {user}")
-        result = await self.handle_op()
-        if result[0]:
-            await interaction.followup.send(f"Op'd `{user}` on {target}!")
-            log_channel = await self.bot.fetch_channel(
-                self.bot.discord_config.log_channel
-            )
-            if isinstance(log_channel, discord.TextChannel):
-                await log_channel.send(
-                    f"`{interaction.user.name}` op'd `{user}` on {target}"
-                )
-        else:
-            await interaction.followup.send(result[1])
+  @command(description="op players")
+  @default_permissions()
+  async def op(
+    self,
+    inter: Interaction,
+    server: Servers,
+    action: Literal["add", "remove"],
+    user: str,
+  ):
+    await inter.response.defer(ephemeral=True)
 
-    @op_commands.command(
-        name="remove", description="Removes a user from a server's op users"
-    )
-    @app_commands.default_permissions(administrator=True)
-    @app_commands.describe(server="target server")
-    @app_commands.choices(server=bot.creative_server_choices)
-    @app_commands.describe(user="target user")
-    async def op_remove(
-        self,
-        interaction: discord.Interaction,
-        server: app_commands.Choice[str],
-        user: str,
-    ):
-        target = server.value
-        await interaction.response.defer(ephemeral=True)
-        await bridge_send(self.bot.servers, target, f"RCON {target} deop {user}")
-        result = await self.handle_op()
-        if result[0]:
-            await interaction.followup.send(f"Deop'd `{user}` on {target}!")
-            log_channel = await self.bot.fetch_channel(
-                self.bot.discord_config.log_channel
-            )
-            if isinstance(log_channel, discord.TextChannel):
-                await log_channel.send(
-                    f"`{interaction.user.name}` deop'd `{user}` on {target}"
-                )
-        else:
-            await interaction.followup.send(result[1])
+    bridge, server_ = server.value
+    command = "op" if action == "add" else "deop"
+    response = await bridge.sendr(f"RCON {server_} {command} {user}")
+
+    if response.startswith("Made"):
+      if "no longer" in response:
+        verb = "Deop'd"
+      else:
+        verb = "Op'd"
+
+      await self.bot.log(
+        f"`{inter.user}` {verb.lower()} `{user}` on {server_.display}",
+      )
+
+      response = f"{verb} {user} on {server_.display}"
+
+    await inter.followup.send(response)
 
 
-async def setup(bot: bot.PropertyBot):
-    await bot.add_cog(Whitelist(bot))
+async def setup(bot: NuggTechBot):
+  await bot.add_cog(Whitelist(bot))
