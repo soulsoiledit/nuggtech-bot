9c9ed2caa3d67737552d92297629730af236a11c
soiledit <no.sleep410@passinbox.com>
2025-06-25 23:15:05 -0500

    update rcon

--- src/cogs/admin/rcon.py
+++ src/cogs/admin/rcon.py
@@ -1,43 +1,30 @@
 import discord
-from discord.ext import commands
 from discord import app_commands
+from discord.ext import commands
 
-import bot
-from bridge import bridge_send
+from bot import NuggTechBot, Servers
 
 
 class Rcon(commands.Cog):
-    def __init__(self, bot: bot.PropertyBot):
-        self.bot = bot
+  def __init__(self, bot: NuggTechBot):
+    self.bot: NuggTechBot = bot
 
-    @app_commands.command(
-        name="rcon",
-        description="Sends an arbitrary rcon command to a creative-enabled server",
-    )
-    @app_commands.default_permissions(administrator=True)
-    @app_commands.describe(server="target server")
-    @app_commands.choices(server=bot.creative_server_choices)
-    @app_commands.describe(command="target server")
-    async def rcon(
-        self,
-        interaction: discord.Interaction,
-        server: app_commands.Choice[str],
-        command: str,
-    ):
-        await interaction.response.defer()
+  @app_commands.command(description="send rcon command")
+  @app_commands.default_permissions()
+  async def rcon(self, inter: discord.Interaction, server: Servers, command: str):
+    await inter.response.defer()
 
-        target = server.value
-        await bridge_send(self.bot.servers, target, f"RCON {target} {command}")
-        result = await self.bot.response_queue.get()
+    bridge, server_ = server.value
+    response = await bridge.sendr(f"RCON {server_} {command}")
 
-        target_server = self.bot.servers[target]
-        embed = discord.Embed(
-            title=f"`/{command}`", description=result, color=target_server.discord_color
-        )
-        embed.set_footer(text=target_server.display_name)
+    embed = discord.Embed(
+      title=f"`/{command}`",
+      description=response,
+      color=server_.color,
+    ).set_footer(text=server_.display)
 
-        await interaction.followup.send(embed=embed)
+    await inter.followup.send(embed=embed)
 
 
-async def setup(bot: bot.PropertyBot):
-    await bot.add_cog(Rcon(bot))
+async def setup(bot: NuggTechBot):
+  await bot.add_cog(Rcon(bot))
