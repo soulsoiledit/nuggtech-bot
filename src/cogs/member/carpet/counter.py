8b9de6a3bdcd956c55a443ef5125ebb7125eb086
soiledit <no.sleep410@passinbox.com>
2025-06-25 23:15:15 -0500

    update counter

--- src/cogs/member/carpet/counter.py
+++ src/cogs/member/carpet/counter.py
@@ -1,135 +1,106 @@
+from enum import Enum
 from typing import Literal
 
-import discord
+from discord import Color, Embed, Interaction
+from discord.app_commands import command
 from discord.ext import commands
-from discord import app_commands
 
-import bot
-from bridge import bridge_send
+from bot import NuggTechBot, Servers
 
-colormap = {
-    "white": 0xCFD5D6,
-    "light_gray": 0x7D7D73,
-    "gray": 0x373A3E,
-    "black": 0x080A0F,
-    "red": 0x8E2121,
-    "orange": 0xE06101,
-    "yellow": 0xF1AF15,
-    "lime": 0x5EA918,
-    "green": 0x495B24,
-    "light_blue": 0x2489C7,
-    "cyan": 0x157788,
-    "blue": 0x2D2F8F,
-    "purple": 0x64209C,
-    "magenta": 0xA9309F,
-    "pink": 0xF5C2E7,
-    "brown": 0x603C20,
-}
 
-CounterArgs = (
-    Literal[
-        "white",
-        "light_gray",
-        "gray",
-        "black",
-        "red",
-        "orange",
-        "yellow",
-        "lime",
-        "green",
-        "light_blue",
-        "cyan",
-        "blue",
-        "purple",
-        "magenta",
-        "pink",
-        "brown",
-    ]
-    | None
-)
+class Counters(Enum):
+  white = Color(0xCFD5D6)
+  light_gray = Color(0x7D7D73)
+  gray = Color(0x373A3E)
+  black = Color(0x080A0F)
+  red = Color(0x8E2121)
+  orange = Color(0xE06101)
+  yellow = Color(0xF1AF15)
+  lime = Color(0x5EA918)
+  green = Color(0x495B24)
+  light_blue = Color(0x2489C7)
+  cyan = Color(0x157788)
+  blue = Color(0x2D2F8F)
+  purple = Color(0x64209C)
+  magenta = Color(0xA9309F)
+  pink = Color(0xF5C2E7)
+  brown = Color(0x603C20)
+  all = None
+
+
+type Actions = Literal["list", "reset"]
 
 
 class Counter(commands.Cog):
-    def __init__(self, bot: bot.PropertyBot):
-        self.bot = bot
+  def __init__(self, bot: NuggTechBot):
+    self.bot: NuggTechBot = bot
 
-    counter_commands = app_commands.Group(
-        name="counter", description="counter commands"
+  async def generic(
+    self,
+    base_command: str,
+    inter: Interaction,
+    server: Servers,
+    counter: Counters = Counters.all,
+    action: Actions = "list",
+  ):
+    pass
+    await inter.response.defer()
+
+    bridge, server_ = server.value
+
+    command_ = base_command
+    color = None
+    if counter.value:
+      command_ += f" {counter.name}"
+      color = counter.value
+    if action == "reset":
+      command_ += " reset"
+
+    response = await bridge.sendr(f"RCON {server_} {command_}")
+
+    response = (
+      response.replace("[X]", "")
+      .replace("- ", "\n- ")
+      .replace("Items for", "\nItems for")
+      .strip()
     )
 
-    async def handle_counter(
-        self, target: str, color: str | None, reset: bool = False
-    ) -> discord.Embed:
-        counter = await self.bot.response_queue.get()
-        counter = counter.replace("[X]", "")
-        counter = counter.replace("/h", "/h\n").replace("/h\n):", "/h):\n")
+    if color is None and response.startswith("Items for"):
+      color = Counters[response.split(maxsplit=3)[2]].value
 
-        colorcmd = f"`/counter {color}" if color else "`/counter"
-        resetcmd = " reset`" if reset else "`"
+    response = [
+      f"**{line}**" if line.startswith("Items for") else line
+      for line in response.splitlines()
+    ]
 
-        embed_color = 0
-        desc = ""
-        counterlines = counter.split("\n")
-        for line in counterlines:
-            if "Items for" in line:
-                embed_color = colormap[line.split()[2]]
-                desc += f"**{line}**\n"
-            else:
-                desc += f"{line}\n"
+    embed = Embed(
+      title=f"`/{command_}`",
+      description="\n".join(response),
+      color=color,
+    ).set_footer(text=server_.display)
 
-        if color:
-            embed_color = colormap[color]
+    await inter.followup.send(embed=embed)
 
-        server = self.bot.servers[target]
-        embed = discord.Embed(
-            title=f"{colorcmd}{resetcmd}", description=desc, color=embed_color
-        )
-        embed.set_footer(text=server.display_name)
+  @command(description="/counter")
+  async def counter(
+    self,
+    inter: Interaction,
+    server: Servers,
+    counter: Counters = Counters.all,
+    action: Actions = "list",
+  ):
+    await self.generic("counter", inter, server, counter, action)
 
-        return embed
-
-    @counter_commands.command(name="list", description="send /counter {counter}")
-    @app_commands.describe(server="target server")
-    @app_commands.choices(server=bot.server_choices)
-    async def counter_info(
-        self,
-        interaction: discord.Interaction,
-        server: app_commands.Choice[str],
-        counter: CounterArgs,
-    ):
-        target = server.value
-        await interaction.response.defer()
-        if counter:
-            await bridge_send(
-                self.bot.servers, target, f"RCON {target} counter {counter}"
-            )
-        else:
-            await bridge_send(self.bot.servers, target, f"RCON {target} counter")
-        await interaction.followup.send(
-            embed=await self.handle_counter(target, counter)
-        )
-
-    @counter_commands.command(name="reset", description="send /counter {counter} reset")
-    @app_commands.describe(server="target server")
-    @app_commands.choices(server=bot.server_choices)
-    async def counter_reset(
-        self,
-        interaction: discord.Interaction,
-        server: app_commands.Choice[str],
-        counter: CounterArgs,
-    ):
-        target = server.value
-        await interaction.response.defer()
-        if counter:
-            await bridge_send(
-                self.bot.servers, target, f"RCON {target} counter {counter} reset"
-            )
-        else:
-            await bridge_send(self.bot.servers, target, f"RCON {target} counter reset")
-        await interaction.followup.send(
-            embed=await self.handle_counter(target, counter, True)
-        )
+  @command(description="/scounter")
+  async def scounter(
+    self,
+    inter: Interaction,
+    server: Servers,
+    counter: Counters = Counters.all,
+    action: Actions = "list",
+  ):
+    await self.generic("scounter", inter, server, counter, action)
 
 
-async def setup(bot: bot.PropertyBot):
-    await bot.add_cog(Counter(bot))
+async def setup(bot: NuggTechBot):
+  await bot.add_cog(Counter(bot))
